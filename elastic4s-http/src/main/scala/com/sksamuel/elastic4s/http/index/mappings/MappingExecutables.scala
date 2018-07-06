a906e71c59f0163a6976cc3bba6989e7dd1acba5
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-07-06 17:03:13 +1000

    fixes mapping error

--- elastic4s-http/src/main/scala/com/sksamuel/elastic4s/http/index/mappings/MappingExecutables.scala
+++ elastic4s-http/src/main/scala/com/sksamuel/elastic4s/http/index/mappings/MappingExecutables.scala
@@ -5,6 +5,7 @@ import com.sksamuel.elastic4s.http.{HttpEntity, HttpExecutable, HttpRequestClien
 import com.sksamuel.elastic4s.indexes.PutMappingBuilderFn
 import com.sksamuel.elastic4s.mappings.{GetMappingDefinition, PutMappingDefinition}
 import org.apache.http.entity.ContentType
+import com.sksamuel.elastic4s.http.ElasticError
 
 import scala.concurrent.Future
 
@@ -15,17 +16,24 @@ trait MappingExecutables {
   implicit object GetMappingHttpExecutable extends HttpExecutable[GetMappingDefinition, Seq[IndexMappings]] {
 
     override def responseHandler: ResponseHandler[Seq[IndexMappings]] = new ResponseHandler[Seq[IndexMappings]] {
-      override def handle(response: HttpResponse) = {
-        val raw = ResponseHandler.fromResponse[Map[String, Map[String, Map[String, Map[String, Any]]]]](response)
-        val raw2 = raw.map {
-          case (index, types) =>
-            val mappings = types("mappings").map {
-              case (tpe, properties) =>
-                tpe -> properties.get("properties").map(_.asInstanceOf[Map[String, Any]]).getOrElse(Map.empty)
-            }
-            IndexMappings(index, mappings)
-        }.toSeq
-        Right(raw2)
+      override def handle(response: HttpResponse): Either[ElasticError, Seq[IndexMappings]] = response.statusCode match {
+        case 201 | 200       =>
+          val raw = ResponseHandler.fromResponse[Map[String, Map[String, Map[String, Map[String, Any]]]]](response)
+          val raw2 = raw.map {
+            case (index, types) =>
+              val mappings = types("mappings").map {
+                case (tpe, properties) =>
+                  tpe -> properties.get("properties").map(_.asInstanceOf[Map[String, Any]]).getOrElse(Map.empty)
+              }
+              IndexMappings(index, mappings)
+          }.toSeq
+          Right(raw2)
+        case _               =>
+          try {
+            Left(ElasticError.parse(response))
+          }catch{
+            case _ : Throwable => sys.error(response.toString)
+          }
       }
     }
 
